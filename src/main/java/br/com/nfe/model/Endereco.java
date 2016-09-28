75d455aa749f5b79803d457f75feed998a173c5e
a1502794 <a1502794@LAB209M14>
2016-09-28 20:33:13 -0300

    Modificações nos models

---
+++ src/main/java/br/com/nfe/model/Endereco.java
@@ -0,0 +1,14 @@
+package br.com.nfe.model;
+
+public class Endereco {
+
+	private Pessoa pessoa;
+	private char tipo;
+	private String endereco;
+	private Integer numero;
+	private String bairro;
+	private String complemento;
+	private String cep;
+	private Municipio municipio;
+	
+}
---
+++ src/main/java/br/com/nfe/model/Telefone.java
@@ -0,0 +1,10 @@
+package br.com.nfe.model;
+
+public class Telefone {
+	
+	private Pessoa pessoa;
+	private char tipo;
+	private String ddd;
+	private String numero;
+	
+}
