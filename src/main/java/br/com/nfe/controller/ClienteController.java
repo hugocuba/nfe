b747440d00f05ccfc59d8c47ea054c5b48e5ee55
a1502794 <a1502794@LAB112M16>
2016-11-22 22:05:17 -0200

    Classe para tratar telefone

--- src/main/java/br/com/nfe/controller/ClienteController.java
+++ src/main/java/br/com/nfe/controller/ClienteController.java
@@ -10,6 +10,10 @@ import br.com.nfe.model.Estado;
 import br.com.nfe.model.Municipio;
 import br.com.nfe.model.Pais;
 import br.com.nfe.model.Telefone;
+import br.com.nfe.utils.Telephone;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -24,27 +28,27 @@ import javax.swing.JTextField;
 public class ClienteController {
 	public boolean salvar(Map<String, JComponent> dados) {
 		boolean salvo = false;
-                
-                Municipio municipio;
-                Estado estado;
-                Pais pais;
+
+		Municipio municipio;
+		Estado estado;
+		Pais pais;
 		Pessoa p;
 
-		//JRadioButton tipo = (JRadioButton) dados.get("tipo");
-		//JCheckBox icms = (JCheckBox) dados.get("icms");
-		//JTextField nome = (JTextField) dados.get("nome");
-		//JTextField ie = (JTextField) dados.get("ie");
-		//JTextField suframa = (JTextField) dados.get("suframa");
+		// JRadioButton tipo = (JRadioButton) dados.get("tipo");
+		// JCheckBox icms = (JCheckBox) dados.get("icms");
+		// JTextField nome = (JTextField) dados.get("nome");
+		// JTextField ie = (JTextField) dados.get("ie");
+		// JTextField suframa = (JTextField) dados.get("suframa");
 		// JTextField email = (JTextField) dados.get("email");
-		//JTextField telefone = (JTextField) dados.get("telefone");
-		//JTextField logradouro = (JTextField) dados.get("logradouro");
-		//JTextField num = (JTextField) dados.get("num");
-		//JTextField bairro = (JTextField) dados.get("bairro");
-		//JTextField complemento = (JTextField) dados.get("complemento");
-		//JTextField cep = (JTextField) dados.get("cep");
-		//JComboBox pais = (JComboBox) dados.get("pais");
-		//JComboBox estado = (JComboBox) dados.get("estado");
-		//JComboBox municipio = (JComboBox) dados.get("municipio");
+		// JTextField telefone = (JTextField) dados.get("telefone");
+		// JTextField logradouro = (JTextField) dados.get("logradouro");
+		// JTextField num = (JTextField) dados.get("num");
+		// JTextField bairro = (JTextField) dados.get("bairro");
+		// JTextField complemento = (JTextField) dados.get("complemento");
+		// JTextField cep = (JTextField) dados.get("cep");
+		// JComboBox pais = (JComboBox) dados.get("pais");
+		// JComboBox estado = (JComboBox) dados.get("estado");
+		// JComboBox municipio = (JComboBox) dados.get("municipio");
 		// JTextField cpf = (JTextField) dados.get("cpf");
 		// JTextField cnpj = (JTextField) dados.get("cnpj");
 
@@ -60,31 +64,37 @@ public class ClienteController {
 
 		p.setNome(((JTextField) dados.get("nome")).getText());
 		p.setEmail(((JTextField) dados.get("email")).getText());
-                p.setInscricaoEstadual(((JTextField) dados.get("ie")).getText());
-                p.setIsencaoIcms(((JCheckBox) dados.get("icms")).isSelected());
-                p.setSuframa(((JTextField) dados.get("suframa")).getText());
-                
-                /*Endereco endereco = new Endereco();
-                endereco.setEndereco(((JTextField) dados.get("logradouro")).getText());
-                endereco.setNumero(Integer.parseInt(((JTextField) dados.get("num")).getText()));
-                endereco.setBairro(((JTextField) dados.get("bairro")).getText());
-                endereco.setComplemento(((JTextField) dados.get("complemento")).getText());
-                endereco.setCep(((JTextField) dados.get("cep")).getText());
-                //endereco.setMunicipio(municipio);
-                endereco.setPessoa(p);*/
-                
-                Telefone telefone = new Telefone();
-                telefone.setDdd("12");
-                telefone.setNumero(((JTextField) dados.get("telefone")).getText());
-                telefone.setPessoa(p);
-                
+		p.setInscricaoEstadual(((JTextField) dados.get("ie")).getText());
+		p.setIsencaoIcms(((JCheckBox) dados.get("icms")).isSelected());
+		p.setSuframa(((JTextField) dados.get("suframa")).getText());
+
+		/*
+		 * Endereco endereco = new Endereco();
+		 * endereco.setEndereco(((JTextField)
+		 * dados.get("logradouro")).getText());
+		 * endereco.setNumero(Integer.parseInt(((JTextField)
+		 * dados.get("num")).getText())); endereco.setBairro(((JTextField)
+		 * dados.get("bairro")).getText());
+		 * endereco.setComplemento(((JTextField)
+		 * dados.get("complemento")).getText()); endereco.setCep(((JTextField)
+		 * dados.get("cep")).getText()); //endereco.setMunicipio(municipio);
+		 * endereco.setPessoa(p);
+		 */
+
+		List<String> tel = new ArrayList<>();
+		tel = Telephone.separarDDD(((JTextField) dados.get("telefone")).getText());
+		Telefone telefone = new Telefone();
+		telefone.setDdd(tel.get(0));
+		telefone.setNumero(tel.get(1));
+		telefone.setPessoa(p);
+
 		Cliente cliente = new Cliente();
 		cliente.setPessoa(p);
 
 		ClienteDAO clienteDAO = new ClienteDAO();
 
 		if (clienteDAO.persist(cliente))
-                    salvo = true;
+			salvo = true;
 
 		return salvo;
 	}
--- src/main/java/br/com/nfe/utils/Telephone.java
+++ src/main/java/br/com/nfe/utils/Telephone.java
@@ -5,12 +5,29 @@
  */
 package br.com.nfe.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author a1502824
  */
 public class Telephone {
-    public static String[] telefone(){
-        
-    }
+	public static List separarDDD(String telefone) {
+		
+		List<String> tel = new ArrayList<>();
+		
+		Pattern pattern = Pattern.compile("\\((\\d+)\\)(\\d+)");
+		
+		Matcher matcher = pattern.matcher(telefone);
+
+		if (matcher.find()) {
+			tel.add(matcher.group(1));
+			tel.add(matcher.group(2));
+		}
+
+		return tel;
+	}
 }
